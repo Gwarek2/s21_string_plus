0f44783227b7a4ed78895e5174d9676ae3913f3a
Fedor <fedorsah@outlook.com>
2022-04-28 12:49:41 +0300

    Completed memchr, memcpy, memcmp, strerror

--- src/s21_string.c
+++ src/s21_string.c
@@ -1,106 +1,121 @@
 #include "s21_string.h"
+#include "s21_errors.h"
 
 
 void *s21_memchr(const void *str, int c, s21_size_t n) {
-    // todo
+    void* result = S21_NULL;
+    int found = 0;
+    for (s21_size_t i = 0; i < n && !found; i++) {
+        if (((char*) str)[i] == c) {
+            result = (void*) (str + i);
+            found = 1;
+        }
+    }
+    return result;
 }
 
 
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
-    // todo
+    s21_size_t i = 0;
+    while (i < n && ((char*) str1)[i] == ((char*) str2)[i])
+        i++;
+    return ((char*) str1)[i] - ((char*) str2)[i];
 }
 
 
 void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
-    // todo
+    for (s21_size_t i = 0; i < n; i++)
+        ((char*) dest)[i] = ((const char*) src)[i];
+    ((char*) dest)[n] = '\0';
+    return dest;
 }
 
 
-void *s21_memmove(void *dest, const void *src, s21_size_t n) {
-    // todo
-}
+// void *s21_memmove(void *dest, const void *src, s21_size_t n) {
+//     // todo
+// }
 
 
-void *s21_memset(void *str, int c, s21_size_t n) {
-    // todo
-}
+// void *s21_memset(void *str, int c, s21_size_t n) {
+//     // todo
+// }
 
 
-char *s21_strcat(char *dest, const char *src) {
-    // todo
-}
+// char *s21_strcat(char *dest, const char *src) {
+//     // todo
+// }
 
 
-char *s21_strncat(char *dest, const char *src, s21_size_t n) {
-    // todo
-}
+// char *s21_strncat(char *dest, const char *src, s21_size_t n) {
+//     // todo
+// }
 
 
-char *s21_strchr(const char *str, int c) {
-    // todo
-}
+// char *s21_strchr(const char *str, int c) {
+//     // todo
+// }
 
 
-int s21_strcmp(const char *str1, const char *str2) {
-    // todo
-}
+// int s21_strcmp(const char *str1, const char *str2) {
+//     // todo
+// }
 
 
-int s21_strncmp(const char *str1, const char *str2, s21_size_t n) {
-    // todo
-}
+// int s21_strncmp(const char *str1, const char *str2, s21_size_t n) {
+//     // todo
+// }
 
 
-char *s21_strcpy(char *dest, const char *src) {
-    // todo
-}
+// char *s21_strcpy(char *dest, const char *src) {
+//     // todo
+// }
 
 
-char *s21_strncpy(char *dest, const char *src, s21_size_t n) {
-    // todo
-}
+// char *s21_strncpy(char *dest, const char *src, s21_size_t n) {
+//     // todo
+// }
 
 
-s21_size_t s21_strcspn(const char *str1, const char *str2) {
-    // todo
-}
+// s21_size_t s21_strcspn(const char *str1, const char *str2) {
+//     // todo
+// }
 
 
 char *s21_strerror(int errnum) {
-    // todo
+    return (char*) error_descrs[errnum];
 }
 
 
-s21_size_t s21_strlen(const char *str) {
-    // todo
-}
+// s21_size_t s21_strlen(const char *str) {
+//     // todo
+// }
 
 
-char *s21_strpbrk(const char *str1, const char *str2) {
-    // todo
-}
+// char *s21_strpbrk(const char *str1, const char *str2) {
+//     // todo
+// }
 
 
-char *s21_strrchr(const char *str, int c) {
-    // todo
-}
+// char *s21_strrchr(const char *str, int c) {
+//     // todo
+// }
 
 
-s21_size_t s21_strspn(const char *str1, const char *str2) {
-    // todo
-}
+// s21_size_t s21_strspn(const char *str1, const char *str2) {
+//     // todo
+// }
 
 
-char *s21_strstr(const char *haystack, const char *needle) {
+// char *s21_strstr(const char *haystack, const char *needle) {
 
-}
-
-
-char *s21_strtok(char *str, const char *delim) {
-
-}
+// }
 
 
-int s21_sprintf(char *str, const char *format, ...) {
+// char *s21_strtok(char *str, const char *delim) {
 
-}
+// }
+
+
+// int s21_sprintf(char *str, const char *format, ...) {
+
+// }
--- src/s21_string_test.c
+++ src/s21_string_test.c
@@ -1,7 +1,173 @@
-#include "s21_string.h"
+#include <stdlib.h>
 #include <string.h>
+#include <check.h>
 
+#include "s21_string.h"
+
+
+START_TEST(test_s21_memchr_normal) {
+    void *str = (void*) "Hello world";
+    void *res1;
+    void *res2;
+    ck_assert_msg((res1 = s21_memchr(str, 111, 10)) == (res2 = memchr(str, 111, 10)),
+                  "Result: %s. Expected: %s.", res1, res2);
+    
+} END_TEST
+
+START_TEST(test_s21_memchr_no_result) {
+    void *str = (void*) "Hello world";
+    void *res1;
+    void *res2;
+    ck_assert_msg((res1 = s21_memchr(str, 107, 11)) == (res2 = memchr(str, 107, 11)),
+                  "Result: %s. Expected: %s.", res1, res2);
+    
+} END_TEST
+
+START_TEST(test_s21_memchr_empty_str) {
+    void *str = (void*) "\n\0";
+    void *res1;
+    void *res2;
+    ck_assert_msg((res1 = s21_memchr(str, 12, 1)) == (res2 = memchr(str, 12, 1)),
+                  "Result: %s. Expected: %s.", res1, res2);
+    
+} END_TEST
+
+Suite * s21_string_memchr_suite(void) {
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("S21_string_memchr");
+    tc_core = tcase_create("Core");
+    tcase_add_test(tc_core, test_s21_memchr_normal);
+    tcase_add_test(tc_core, test_s21_memchr_no_result);
+    tcase_add_test(tc_core, test_s21_memchr_empty_str);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+START_TEST(test_s21_memcmp_normal) {
+    void *str1 = (void*) "Hello world";
+    void *str2 = (void*) "Hello world";
+    int res1;
+    int res2;
+    ck_assert_msg((res1 = s21_memcmp(str1, str2, 11)) == (res2 = memcmp(str1, str2, 11)),
+                  "Result: %i. Expected: %i.", res1, res2);
+} END_TEST
+
+START_TEST(test_s21_memcmp_not_equal) {
+    void *str1 = (void*) "Hello world";
+    void *str2 = (void*) "HELLO WORLD";
+    int res1;
+    int res2;
+    ck_assert_msg((res1 = s21_memcmp(str1, str2, 11)) == (res2 = memcmp(str1, str2, 11)),
+                  "Result: %i. Expected: %i.", res1, res2);
+} END_TEST
+
+START_TEST(test_s21_memcmp_empty_str) {
+    void *str1 = (void*) "\0";
+    void *str2 = (void*) "\0";
+    int res1;
+    int res2;
+    ck_assert_msg((res1 = s21_memcmp(str1, str2, 1)) == (res2 = memcmp(str1, str2, 1)),
+                  "Result: %i. Expected: %i.", res1, res2);
+} END_TEST
+
+Suite * s21_string_memcmp_suite(void) {
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("S21_string_memcmp");
+    tc_core = tcase_create("Core");
+    tcase_add_test(tc_core, test_s21_memcmp_normal);
+    tcase_add_test(tc_core, test_s21_memcmp_not_equal);
+    tcase_add_test(tc_core, test_s21_memcmp_empty_str);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+START_TEST(test_s21_memcpy_full) {
+    const void *str = (void*) "Hello world";
+    char buff1[20]; char buff2[20];
+    void *buffer1 = (void*) buff1;
+    void *buffer2 = (void*) buff2;
+    s21_memcpy(buffer1, str, 11);
+    memcpy(buffer2, str, 11);
+    ck_assert_msg(memcmp(buffer1, buffer2, 11) == 0, "Result: %s. Expected: %s.", buffer1, buffer2);
+} END_TEST
+
+START_TEST(test_s21_memcpy_partial) {
+    const void *str = (void*) "Text to copy";
+    char buff1[20]; char buff2[20];
+    void *buffer1 = (void*) buff1;
+    void *buffer2 = (void*) buff2;
+    s21_memcpy(buffer1, str, 6);
+    memcpy(buffer2, str, 6);
+    ck_assert_msg(memcmp(buffer1, buffer2, 6) == 0, "Result: %s. Expected: %s.", buffer1, buffer2);
+} END_TEST
+
+START_TEST(test_s21_memcpy_empty) {
+    const void *str = (void*) "\0";
+    char buff1[20]; char buff2[20];
+    void *buffer1 = (void*) buff1;
+    void *buffer2 = (void*) buff2;
+    s21_memcpy(buffer1, str, 1);
+    memcpy(buffer2, str, 1);
+    ck_assert_msg(memcmp(buffer1, buffer2, 1) == 0, "Result: %s. Expected: %s.", buffer1, buffer2);
+} END_TEST
+
+Suite * s21_string_memcpy_suite(void) {
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("S21_string_memcpy");
+    tc_core = tcase_create("Core");
+    tcase_add_test(tc_core, test_s21_memcpy_full);
+    tcase_add_test(tc_core, test_s21_memcpy_partial);
+    tcase_add_test(tc_core, test_s21_memcpy_empty);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+START_TEST(test_s21_strerror_1) {
+} END_TEST
+
+START_TEST(test_s21_strerror_2) {
+} END_TEST
+
+START_TEST(test_s21_strerror_3) {
+} END_TEST
+
+Suite * s21_string_strerror_suite(void) {
+    Suite *s;
+    TCase *tc_core;
+
+    s = suite_create("S21_string_strerror");
+    tc_core = tcase_create("Core");
+    tcase_add_test(tc_core, test_s21_strerror_1);
+    tcase_add_test(tc_core, test_s21_strerror_2);
+    tcase_add_test(tc_core, test_s21_strerror_3);
+    suite_add_tcase(s, tc_core);
+}
 
 int main() {
-    return 0;
+    int number_failed;
+    Suite *s_memchr;
+    Suite *s_memcmp;
+    Suite *s_memcpy;
+    SRunner *sr;
+
+    s_memchr = s21_string_memchr_suite();
+    s_memcmp = s21_string_memcmp_suite();
+    s_memcpy = s21_string_memcpy_suite();
+    sr = srunner_create(s_memchr);
+    srunner_add_suite(sr, s_memcmp);
+    srunner_add_suite(sr, s_memcpy);
+
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
