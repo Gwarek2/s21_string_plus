27237e3b2fe7576fbcbda7c44f38ec7cde648e2f
Werlag Deckard <werlagde@su-f6.msk.21-school.ru>
2022-05-14 12:18:22 +0300

    Improved strerr function

--- src/lib/s21_strerr.c
+++ src/lib/s21_strerr.c
@@ -7,13 +7,10 @@ char *s21_strerror(int errnum) {
     if (errnum >= 0 && errnum < ERROR_COUNT) {
         s21_strcpy(buff, ERROR_LIST[errnum]);
     } else {
-        char num[5];
-        struct f_params params;
-        _set_default_params(&params);
-        int arg = errnum < 0 ? -errnum : errnum;
-        itoa(arg, num, 10, errnum < 0, params);
-        s21_strcpy(buff, UNKNOWN_ERROR);
-        s21_strcpy(buff + s21_strlen(UNKNOWN_ERROR), num);
+        // char num[100];
+        s21_sprintf(buff, "%s%d", UNKNOWN_ERROR, errnum);
+        // s21_strcpy(buff, UNKNOWN_ERROR);
+        // s21_strcpy(buff + s21_strlen(UNKNOWN_ERROR), num);
     }
     return buff;
 }
