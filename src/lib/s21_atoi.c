353884c13690b46fd5432dd905a1cb8fe2e8c99c
Werlag Deckard <werlagde@su-f6.msk.21-school.ru>
2022-05-15 21:06:59 +0300

    Small changes in atoi function

--- src/lib/s21_atoi.c
+++ src/lib/s21_atoi.c
@@ -1,20 +1,22 @@
 #include "s21_string.h"
 
 
-int s21_atoi(char *str) {
-    int num = 0;
-    int dec = 1;
+long long s21_atoi(char *str) {
+    long long num = 0;
+    long long dec = 1;
 
-    char *cursor = str + s21_strlen(str) - 1;
-    while (str < cursor) {
-        num += (*cursor - 48) * dec;
-        dec *= 10;
-        cursor--;
+    if (*str) {
+        char *cursor = str + s21_strlen(str) - 1;
+        while (str < cursor) {
+            num += (*cursor - 48) * dec;
+            dec *= 10;
+            cursor--;
+        }
+
+        if (*cursor == '-')
+            num = -num;
+        else
+            num += (*cursor - 48) * dec;
     }
-
-    if (*cursor == '-')
-        num = -num;
-    else
-        num += (*cursor - 48) * dec;
     return num;
 }
