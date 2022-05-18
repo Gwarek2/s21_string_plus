df4ecf003fb6f9de829b2248e0ef8a1b0308c1a4
Fedor <fedorsah@outlook.com>
2022-05-18 11:01:35 +0300

    Corrected insert function

--- src/lib/s21_insert.c
+++ src/lib/s21_insert.c
@@ -8,7 +8,8 @@ void *s21_insert(const char *src, const char *str, s21_size_t start_index) {
         if (start_index <= src_len) {
             if (str == S21_NULL) {
                 returned_string = calloc(src_len + 1, sizeof(char));
-                s21_strcpy(returned_string, src);
+                if (returned_string != S21_NULL)
+                    s21_strcpy(returned_string, src);
             } else {
                 s21_size_t str_len = s21_strlen(str);
                 returned_string = calloc(src_len + str_len + 1, sizeof(char));
